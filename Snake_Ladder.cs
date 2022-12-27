3a60c2adb4cda1b22834c1d8be0787ef3b216c19
Shubham20006 <shubhamchaudhari1506@gmail.com>
2022-12-27 23:29:27 +0530

    Which player won the game

--- Snake_Ladder.cs
+++ Snake_Ladder.cs
@@ -10,15 +10,25 @@ namespace Snake_And_Ladder_Game
     public class Snake_Ladder
     {
         public static void snakeLadder()
-        {
+        {   
+            // Constant
             int num;
+            int select_option;
             int START_POSITION = 0;
             int END_POSITION = 100;
+            int NO_PLAY_OPTION = 0;
+            int LADDER_OPTION = 1;
+            int SNAKE_OPTION = 2;
+
+            //Variables
             int playerPosition = 0;
             int previousPlayerPosition = 0;
             int updatedPlayerPosition = 0;
             int countDiceRoll = 0;
+            int playerOnePosition = 0;
+            int playerTwoPosition = 0;
 
+            //Function for rolling dice
             void rollDice()
             {
                 countDiceRoll = (countDiceRoll + 1);
@@ -26,13 +36,11 @@ namespace Snake_And_Ladder_Game
                 num = random.Next(6);
                 option();
             }
+            //FUNCTION FOR SELECT OPTION NO PLAY, LADDER AND SNAKE
             void option()
             {
-                int NO_PLAY_OPTION = 0;
-                int LADDER_OPTION = 1;
-                int SNAKE_OPTION = 2;
                 Random R = new Random();
-                int select_option = R.Next(3);
+                 select_option = R.Next(3);
 
                 if (select_option == NO_PLAY_OPTION)
                 {
@@ -47,12 +55,12 @@ namespace Snake_And_Ladder_Game
                     snake();
                 }
             }
-
+            //FUNCTION FOR NO PLAY
             void noPlay()
             {
                 playerPosition = previousPlayerPosition;
             }
-
+            //FUNCTION FOR LADDER
             void ladder()
             {
                 updatedPlayerPosition = (previousPlayerPosition + num);
@@ -71,7 +79,7 @@ namespace Snake_And_Ladder_Game
 
                 previousPlayerPosition = playerPosition;
             }
-
+           // FUNCTION FOR SNAKE
             void snake()
             {
                 updatedPlayerPosition = (previousPlayerPosition - num);
@@ -86,11 +94,41 @@ namespace Snake_And_Ladder_Game
 
                 previousPlayerPosition = playerPosition;
             }
+            //FUNCTION FOR PLAYER ONE
+            void playerOne()
+            {
+                rollDice();
+            }
+            //FUNCTION FOR PLAYER TWO
+            void playerTwo()
+            {
+                rollDice();
+            }
 
             while (playerPosition >= START_POSITION && playerPosition < END_POSITION)
             {
-                rollDice();
-                Console.WriteLine("Position After Every Roll Dice :-  " + playerPosition);
+                playerOne();
+                if (select_option == LADDER_OPTION)
+                {
+                    playerOne();
+                }
+                playerOnePosition =playerPosition;
+                playerPosition =playerTwoPosition;
+                Console.WriteLine("Player One,Position After Every Roll Dice :-  " + playerOnePosition);
+                if (playerOnePosition == END_POSITION)
+                {
+                    Console.WriteLine(".........*** Player One Win ***........");
+                    break;
+                }
+                playerTwo();
+                playerTwoPosition = playerPosition;
+                playerPosition = playerOnePosition;
+                Console.WriteLine("Player Two,Position After Every Roll Dice :-  " + playerTwoPosition);
+                if (playerTwoPosition == END_POSITION)
+                {
+                    Console.WriteLine(".........*** Player Two Win ***........");
+                    break;
+                }              
             }
             Console.WriteLine("Total Number Of Dice Roll :- " + countDiceRoll);
         }   
