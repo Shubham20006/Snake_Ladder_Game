649ea36e667abdf8aa5778fcc6ffd114cea44848
Shubham20006 <shubhamchaudhari1506@gmail.com>
2022-12-27 20:05:21 +0530

    User welcome

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Snake_And_Ladder_Game
+{
+    public class SnakeLader
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake And Ladder Game");
+        }
+    }
+}
