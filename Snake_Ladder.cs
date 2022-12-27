51e6ac0ed7c93c15a8dc7f4cb89277a0a7556bcb
Shubham20006 <shubhamchaudhari1506@gmail.com>
2022-12-27 22:03:22 +0530

    Check for a Option

--- Snake_Ladder.cs
+++ Snake_Ladder.cs
@@ -3,33 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Snake_And_Ladder_Game
 {
     public class Snake_Ladder
     {
-        int num;
-        int START_POSITION = 0;
-        int END_POSITION = 100;
-        int playerPosition = 0;
-        int previousPlayerPosition = 0;
-        int updatedPlayerPosition = 0;
-        int countDiceRoll = 0;
-        public int SnakeLadder()
-        {
-           // Random random = new Random();
-           
-            while (playerPosition >= START_POSITION && playerPosition <= END_POSITION)
+        public static void snakeLadder() {
+            int num;
+            int START_POSITION = 0;
+            int END_POSITION = 100;
+            int playerPosition = 0;
+            int previousPlayerPosition = 0;
+            int updatedPlayerPosition = 0;
+            int countDiceRoll = 0;
+
+            void rollDice()
             {
-                Random random = new Random();
-                 num = random.Next(6);
-                Console.WriteLine(num);
+                Random Y = new Random();
+                num = Y.Next(6);
+                option();
+            }
+            void option()
+            {
+                int NO_PLAY_OPTION = 0;
+                int LADDER_OPTION = 1;
+                int SNAKE_OPTION = 2;
+                Random R = new Random();
+                int select_option = R.Next(3);
+
+                if (select_option == NO_PLAY_OPTION)
+                {
+                    playerPosition = previousPlayerPosition;
+                    rollDice();
+                }
+                else if (select_option == LADDER_OPTION)
+                {
+                    updatedPlayerPosition = (previousPlayerPosition + num);
+                }
+                else
+                {
+                    updatedPlayerPosition = (previousPlayerPosition - num);
+                }
             }
 
-            return num;
+                while (playerPosition >= START_POSITION && playerPosition <= END_POSITION)
+                {
+                    rollDice();
+                }
+            
         }
-
-
     }
-        }
+}
     
