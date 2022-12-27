bd961dcbe71390b626eba304203fedda010b0f24
Shubham20006 <shubhamchaudhari1506@gmail.com>
2022-12-27 20:58:25 +0530

    Player rolls the die

--- Snake_Ladder.cs
+++ Snake_Ladder.cs
@@ -8,18 +8,28 @@ namespace Snake_And_Ladder_Game
 {
     public class Snake_Ladder
     {
-       int START_POSITION=0;
-       int END_POSITION=100;
-       int playerPosition=0;
-       int previousPlayerPosition=0;
-       int updatedPlayerPosition=0;
-       int countDiceRoll=0;
-        public static void SnakeLadder()
+        int num;
+        int START_POSITION = 0;
+        int END_POSITION = 100;
+        int playerPosition = 0;
+        int previousPlayerPosition = 0;
+        int updatedPlayerPosition = 0;
+        int countDiceRoll = 0;
+        public int SnakeLadder()
         {
-            Console.WriteLine("Let's Start the Game");
+           // Random random = new Random();
+           
+            while (playerPosition >= START_POSITION && playerPosition <= END_POSITION)
+            {
+                Random random = new Random();
+                 num = random.Next(6);
+                Console.WriteLine(num);
+            }
+
+            return num;
         }
-          
-    
-        
-        }
+
+
     }
+        }
+    
